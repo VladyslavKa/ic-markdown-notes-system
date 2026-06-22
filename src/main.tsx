da48ff0780f01cbc19a242645da316fe87fa4090
VladyslavKa <kashuba.vlad@gmail.com>
2026-06-22 11:21:49 +0300

    feat: added theme toggle

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router/dom";
 import { router } from "./router";
+import { ThemeToggle } from "./shared/ui/ThemeToggle";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <ThemeToggle />
     <RouterProvider router={router} />
   </StrictMode>,
 );
